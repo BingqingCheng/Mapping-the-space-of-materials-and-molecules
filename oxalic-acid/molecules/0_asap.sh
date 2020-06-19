f158435282f9e19109d54ffe7a4e4a00a55b4e75
Simon Wengert <simonwengert@web.de>
2020-06-19 13:37:02 +0200

    ENH: updated scripts and data for plot including relative energies for individual MDs

--- oxalic-acid/molecules/0_asap.sh
+++ oxalic-acid/molecules/0_asap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# get xyz containing 56 relaxed conformers and 2x3000 conformers from two MD trajectories
-tar xf LocalOpt56__FirstMD3000__SecondMD3000.xyz.tar.xz
+# get xyz containing 56 relaxed configurations and 7x3000 conformers from 7 MD trajectories
+tar xf LocalOpt56__MD3000x7.xyz.tar.xz
 
-# generate descriptors, perform spares kPCA on locally relaxed conformers and project remaining conformers
-asap gen_desc -f LocalOpt56__FirstMD3000__SecondMD3000.xyz --no-periodic soap -c 2.5 -n 8 -l 8 -g 0.25 -z 4 --crossover
+# generate descriptors, perform spares kPCA on locally relaxed configurations and project remaining conformers
+asap gen_desc -f LocalOpt56__MD3000x7.xyz --no-periodic soap -c 2.5 -n 8 -l 8 -g 0.25 -z 4 --crossover
 asap map -f ASAP-desc.xyz -dm '[*]' skpca -s sequential -n 56 --no-scale
--- oxalic-acid/molecules/1_plot.py
+++ oxalic-acid/molecules/1_plot.py
@@ -20,37 +20,34 @@ def get_arrays(key, atoms):
 
 ## Collect the data
 atoms = ase.io.read('ASAP-lowD-map.xyz', ':')
-local_opt = atoms[:56]
-first_md = atoms[56:3056]
-second_md = atoms[3056:]
 
-lopt_pcs = get_info_values('skpca-d-10', local_opt)
-fmd_pcs = get_info_values('skpca-d-10', first_md)
-smd_pcs = get_info_values('skpca-d-10', second_md)
-
-lopt_pc1, lopt_pc2 = lopt_pcs[:, 0], lopt_pcs[:, 1]
-fmd_pc1, fmd_pc2 = fmd_pcs[:, 0], fmd_pcs[:, 1]
-smd_pc1, smd_pc2 = smd_pcs[:, 0], smd_pcs[:, 1]
-
-lopt_energies = get_info_values('E_dftbplus_ts_relative', local_opt)
+pcs = get_info_values('skpca-d-10', atoms)
+energies = get_info_values('E_relative', atoms)
 
 
-## Plotting
+## Plotting (PC1 vs PC2)
+fig, axl = plt.subplots()
 
-# marker size scaling of locally relaxed conformers according to stability
-size_range = [30, 150]
-inv_energy = 1./(lopt_energies + 0.1)
-inv_energy_range = [np.min(inv_energy), np.max(inv_energy)]
-sizes = size_range[0] + (size_range[1] - size_range[0])/(inv_energy_range[1] - inv_energy_range[0]) * inv_energy
+# define limits for color-coding
+ll, ul = np.min(energies), np.max(energies)
 
-# locally relaxed conformers
-pcmm = plt.scatter(lopt_pc1, lopt_pc2, zorder=2, s=sizes, c=lopt_energies, cmap='gnuplot', edgecolor='k', linewidths=0.2)
-plt.colorbar(pcmm).set_label('Relative Energy / eV', rotation=270, labelpad=20)
+# MD which stays in its initial minimum (with transparency)
+axl.scatter(pcs[12056:15056, 0], pcs[12056:15056, 1], c=energies[12056:15056],
+            alpha=0.4, zorder=1, s=10, cmap='gnuplot', edgecolor='k', linewidths=0.2, vmin=ll, vmax=ul)
+# remaining 6 MDs
+axl.scatter(np.concatenate((pcs[56:12056, 0], pcs[15056:21056, 0])),
+            np.concatenate((pcs[56:12056, 1], pcs[15056:21056, 1])),
+            c=np.concatenate((energies[56:12056], energies[15056:21056])),
+            alpha=1.0, zorder=1, s=10, cmap='gnuplot', edgecolor='k', linewidths=0.2, vmin=ll, vmax=ul)
+# locally relaxed configurations
+pcmm = axl.scatter(pcs[:56, 0], pcs[:56, 1],
+                   zorder=2, s=50, c=energies[:56],
+                   cmap='gnuplot', edgecolor='k', linewidths=0.2, vmin=ll, vmax=ul)
+plt.colorbar(pcmm, ax=axl).set_label('Relative Energy / eV', rotation=270, labelpad=20)
 
-# molecular dynamics data from two trajectories initialized in different basins
-plt.plot(fmd_pc1, fmd_pc2, c='lightgray', alpha=0.8, lw=2, ls=':', zorder=1)
-plt.plot(smd_pc1, smd_pc2, c='darkgray', alpha=0.8, lw=2, ls=':', zorder=1)
-
+# first step of MD starting from high energy configuration
+axl.plot(pcs[18056:18058, 0], pcs[18056:18058, 1],
+          alpha=0.8, c='darkgray', lw=2, ls=':', zorder=1)
 
 plt.tick_params(axis='both', which='both',
     bottom=False, top=False, right=False, left=False,
